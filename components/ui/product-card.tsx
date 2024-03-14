90fe3421e949dea14391572d2988c054f5dceb80
Varun Gotmare <varunpgotmare@gmail.com>
2024-03-14 23:05:46 +0530

    final

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -4,6 +4,10 @@ import Image from "next/image";
 import IconButton from "./icon-button";
 import {  Expand, ShoppingCart } from "lucide-react";
 import Currency from "./currency";
+import { useRouter } from "next/navigation";
+import PreviewModal from "../preview-modal";
+import usePreviewModal from "@/hooks/use-preview-modal";
+import useCart from "@/hooks/use-cart";
 
 interface ProductCard{
     data: Product;
@@ -11,14 +15,28 @@ interface ProductCard{
 const ProductCard: React.FC<ProductCard> = (
     {data}
 ) => {
+    const previewModal = usePreviewModal();
+    const cart = useCart();
+    const onPreview: React.MouseEventHandler<HTMLButtonElement>=(event)=>{
+        event.stopPropagation();
+        previewModal.onOpen(data);
+    }
+    const router = useRouter();
+    const handleClick=()=>{
+        router.push(`/product/${data.id}`);
+    }
+    const onAddToCart: React.MouseEventHandler<HTMLButtonElement>=(event)=>{
+        event.stopPropagation();
+        cart.addItem(data);
+    }
     return ( 
-    <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+    <div onClick={handleClick} className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
         <div className="aspect-square rounded-xl bg-gray-100 relative">
             <Image src={data?.images?.[0].url} alt="Image" fill className="aspect-square object-cover rounded-md"/>
             <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
                 <div className="flex gap-x-6 justify-center">
-                    <IconButton onClick={()=>{}} icon={<Expand size={20} className="text-gray-600"/>}/>
-                    <IconButton onClick={()=>{}} icon={<ShoppingCart size={20} className="text-gray-600"/>}/>
+                    <IconButton onClick={onPreview} icon={<Expand size={20} className="text-gray-600"/>}/>
+                    <IconButton onClick={onAddToCart} icon={<ShoppingCart size={20} className="text-gray-600"/>}/>
                 </div>
             </div>
         </div>
